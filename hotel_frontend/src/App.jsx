88a03394f3d9462d96fb3de49fda718110417dce
pritisharma1511 <ps0130815@gmail.com>
2025-11-03 19:14:15 +0530

    routes created

--- hotel_frontend/src/App.jsx
+++ hotel_frontend/src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import './App.css'
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import {Toaster} from 'react-hot-toast'
+import Home from './pages/Home'
+import Success from './pages/Success'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
-        <Route path="/about" element={<div>About</div>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
       <Toaster />
     </Router>
