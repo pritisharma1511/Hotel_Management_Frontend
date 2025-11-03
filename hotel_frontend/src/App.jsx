ac5bb575eb91f1a35c4289c23b354c2bbb29b394
pritisharma1511 <ps0130815@gmail.com>
2025-11-03 19:06:28 +0530

    home page created

--- hotel_frontend/src/App.jsx
+++ hotel_frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import {Toaster} from 'react-hot-toast'
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
         <Route path="/" element={<div>Home</div>} />
         <Route path="/about" element={<div>About</div>} />
       </Routes>
+      <Toaster />
     </Router>
+   
   )
 }
 
